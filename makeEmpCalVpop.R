03d0dc314bf49e71f9c7e9a401e3e79425af7a2f
Claus Wilke <wilke@austin.utexas.edu>
2013-09-20 17:23:11 -0500

    Minor change to axis label, SA -> ASA.

--- makeEmpCalVpop.R
+++ makeEmpCalVpop.R
@@ -14,6 +14,6 @@ par(mgp=c(3,2,0))
 plot(x=d$pop, y=d$SA_diff, log="x", xlab= " ", ylab=" ", las=1, pch=16, cex.axis=3.0, xaxt='n', xlim= c(1,5050), cex=2, bty="l")
 ##points(x=d$pop, y=d$SA_diff, pch=16, col="gray"
 axis(1, c(1,5,50, 500, 5000), tck=-.01, cex.axis=3.0) 
-mtext(expression(paste(Delta, "(Maximum SA per Bin)" )), side=2, line=6.5, cex=3.0)
+mtext(expression(paste(Delta, "(Maximum ASA per Bin)" )), side=2, line=6.5, cex=3.0)
 mtext("Number of RSA Values per bin", side=1, line=4.5, cex=3.0)
 dev.off()
